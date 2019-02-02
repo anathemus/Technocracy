962359a60841ce04b7edaca8772e82acc397c18c
Benjamin Burgess <anathemus@gmail.com>
2019-02-02 12:46:35 -0500

    FIRST WORKING VERSION!!!!!
    Vesion works!

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+
+	public Button playButton;
+	// Use this for initialization
+	void Start () {
+		Button button = playButton.GetComponent<Button>();
+		button.onClick.AddListener(TaskOnClick);
+	}
+
+	//What happens when you click the playButton;
+	void TaskOnClick(){
+		SceneManager.LoadSceneAsync("MainGame");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
